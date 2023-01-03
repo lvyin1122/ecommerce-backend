25a3e40fff52356c3c8aa71abe6ad7cc24cf6857
lvyin1122 <khiwhyzqd@outlook.com>
2023-01-03 16:48:04 +0800

    new

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,20 +1,16 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 
-
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-    if (token == null) return res.sendStatus(401);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
 
-    console.log(JWT_SECRET)
-    console.log(token)
-
-    jwt.verify(token, JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-}
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 
 module.exports = verifyToken;
